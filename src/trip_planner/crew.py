ad464fd37f7f53a1557bfa01d177686ecccff9be
JairRamosG <ramosgonzalezjair@gmail.com>
2026-06-03 12:45:00 -0600

    uso de API_KEY anthropic

--- src/trip_planner/crew.py
+++ src/trip_planner/crew.py
@@ -1,6 +1,7 @@
 from crewai import Agent, Crew, Process, Task
 from crewai.project import CrewBase, agent, crew, task
 from crewai.agents.agent_builder.base_agent import BaseAgent
+from crewai_tools import SerperDevTool, ScrapeWebsiteTool
 
 
 @CrewBase
@@ -9,33 +10,55 @@ class TripPlanner():
 
     agents: list[BaseAgent]
     tasks: list[Task]
-
+# --- Agentes ------------------------------------------------------------------
     @agent
-    def researcher(self) -> Agent:
+    def expert_travel_agent(self) -> Agent:
         return Agent(
-            config=self.agents_config['researcher'], # type: ignore[index]
+            config=self.agents_config['expert_travel_agent'],
+            tools = [SerperDevTool()],
             verbose=True
         )
 
     @agent
-    def reporting_analyst(self) -> Agent:
+    def city_selection_expert(self) -> Agent:
         return Agent(
-            config=self.agents_config['reporting_analyst'], # type: ignore[index]
+            config=self.agents_config['city_selection_expert'],
+            tools = [SerperDevTool()], 
             verbose=True
         )
 
+    @agent
+    def local_tour_guide(self) -> Agent:
+        return Agent(
+            config=self.agents_config['local_tour_guide'],
+            tools = [SerperDevTool(), ScrapeWebsiteTool()], 
+            verbose=True
+        )
+
+# --- Tasks ------------------------------------------------------------------
+
     @task
-    def research_task(self) -> Task:
+    def city_selection(self) -> Task:
         return Task(
-            config=self.tasks_config['research_task'], # type: ignore[index]
+            config=self.tasks_config['city_selection'], # type: ignore[index]
         )
 
     @task
-    def reporting_task(self) -> Task:
+    def gather_city_info(self) -> Task:
         return Task(
-            config=self.tasks_config['reporting_task'], # type: ignore[index]
-            output_file='report.md'
+            config=self.tasks_config['gather_city_info'], # type: ignore[index]
+            output_file='local_guide_report.md'
         )
+    
+    @task
+    def itinerary_planning(self) -> Task:
+        return Task(
+            config=self.tasks_config['itinerary_planning'], # type: ignore[index]
+            output_file='itinerary.md'
+        )
+
+
+# --- Crew ------------------------------------------------------------------
 
     @crew
     def crew(self) -> Crew:
